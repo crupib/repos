98c78d262a06dcae08298f915fefbe0db07221eb
Bill <crupib@gmail.com>
2023-03-18 11:27:45 -0400

    chapt 1 finished

--- auto_keyword/auto_keyword.cpp
+++ auto_keyword/auto_keyword.cpp
@@ -5,9 +5,27 @@
 using namespace std;
 constexpr double GetPi() { return 22.0 / 7; }
 constexpr double TwicePi() { return 2 * GetPi(); }
-
 int main()
 {
+   
+    enum RainbowColors
+    {
+        Violet = 0,
+        Indigo,
+        Blue,
+        Green,
+        Yellow,
+        Orange,
+        Red
+    };
+    enum CardinalDirections
+    {
+        North = 25,
+        South,
+        East,
+        West
+    };
+
     const double Pi = 22.0 / 7;
     
     auto Flag = true;
@@ -21,5 +39,16 @@ int main()
     cout << "my pi = " << mypi << endl;
     auto twicepi = TwicePi();
     cout << "my 2 * pi = " << twicepi << endl;
+    RainbowColors MyWorlds = Blue;
+    cout << "Displaying directions and their symbolic values" << endl;
+    cout << "North: " << North << endl;
+    cout << "South: " << South << endl;
+    cout << "East: " << East << endl;
+    cout << "West: " << West << endl;
+    CardinalDirections WindDirection = South;
+    cout << "Variable Windirection = " << WindDirection << endl;
+    unsigned short MyShortInt = 0;
+    MyShortInt = MyShortInt - 1;
+    cout << MyShortInt << endl;
     return 0;
 }
