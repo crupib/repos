538ce8db6163f85a7082bb154c220f7d3bdb6f0a
Bill <crupib@gmail.com>
2023-04-15 13:26:59 -0400

    Vectors

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -2,13 +2,14 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
     const int ARRAY_LENGTH = 5;
     int MyNumbers[ARRAY_LENGTH] = {0};
-    cout << "Enter index of the element to be changed: ";
+  /* cout << "Enter index of the element to be changed: ";
     int nElementIndex = 0;
     cin >> nElementIndex;
     cout << "Enter new value: ";
@@ -19,5 +20,29 @@ int main()
     cout << "Fourth element at index 3: " << MyNumbers[3] << endl;
     cout << "Fifth element at index 4: " << MyNumbers[4] << endl;
     cout << "Hello World!" << endl;
-    return 0;
+*/
+    vector <int> DynArrNum(3);
+    DynArrNum[0] = 365;
+    DynArrNum[1] = -421;
+    DynArrNum[2] = 789;
+    cout << "Number of integers in array " << DynArrNum.size() << endl;
+    cout << "Enter another number for the array" << endl;
+    int AnotherNum = 0;
+    cin >> AnotherNum;
+    DynArrNum.push_back(AnotherNum);
+    cout << "Number of integers in array: " << DynArrNum.size() << endl;
+    cout << "Last element in array" << endl;
+    cout << DynArrNum[DynArrNum.size() - 1] << endl;
+    int ThreeRowsThreeColumns[3][3] = \
+         {{-501, 206, 2011}, { 989, 101, 206 }, { 303, 456, 596 }};
+    cout << "Row 0: " << ThreeRowsThreeColumns[0][0] << " " \
+         << ThreeRowsThreeColumns[0][1] << " " \
+         << ThreeRowsThreeColumns[0][2] << endl;
+    cout << "Row 1: " << ThreeRowsThreeColumns[1][0] << " " \
+         << ThreeRowsThreeColumns[1][1] << " " \
+         << ThreeRowsThreeColumns[1][2] << endl;
+    cout << "Row 2: " << ThreeRowsThreeColumns[2][0] << " "\
+         << ThreeRowsThreeColumns[2][1] << " " \
+         << ThreeRowsThreeColumns[2][2] << endl;
+     return 0;
 }
