1debab269f4927398d869ab818cda2392d06652c
Bill <crupib@gmail.com>
2023-04-29 11:59:57 -0400

    templates

---
+++ templates_1/templates_1.cpp
@@ -0,0 +1,18 @@
+// templates_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "max1.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    double f1 = 3.4;
+    double f2 = -6.7;
+
+    std::cout << "max(f1,f2): " << ::max(f1, f2) << std::endl;
+    std::string s1 = "mathematics";
+    std::string s2 = "math";
+    std::cout << "max(s1,s2): " << ::max(s1, s2) << std::endl;
+}
