b8a899daf1efc194fa1e4d32e3e2cca8e4128ac0
Bill <crupib@gmail.com>
2023-04-30 14:10:11 -0400

    get rid of .vs

--- templates_1/max1.hpp
+++ templates_1/max1.hpp
@@ -3,3 +3,11 @@ T max(T a, T b)
 {
 	return b < a ? a : b;
 }
+template<typename T>
+T foo(T t)
+{
+	undeclared();
+	undeclared(t);
+	static_assert (sizeof(int) > 10, "int too small");
+	static_assert (sizeof(T) > 10, "T  too small");
+}
